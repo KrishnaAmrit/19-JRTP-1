82529644bcafb6e091aacdc03393b8c5de3edf4b
ak203oops@gmail.com <ak203oops@gmail.com>
2022-03-09 23:01:57 +0530

    addding new class

---
+++ src/main/java/com/agamit/JavaUtils.java
@@ -0,0 +1,19 @@
+package com.agamit;
+
+public class JavaUtils {
+	public static void main(String[] args) {
+		System.out.println("hi");
+		
+	}
+private void sysout() {
+	
+	int a=10;
+	int b=30;
+	
+
+}
+	
+	
+	
+
+}
