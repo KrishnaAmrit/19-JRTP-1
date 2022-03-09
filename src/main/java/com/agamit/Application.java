ccb87352b4ed99dc3da1188c5239d05b2831fd71
ak203oops@gmail.com <ak203oops@gmail.com>
2022-03-09 22:49:26 +0530

    logic

--- src/main/java/com/agamit/Application.java
+++ src/main/java/com/agamit/Application.java
@@ -9,5 +9,14 @@ public class Application
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("hello Agam");
+        
+        
+        int a=10;
+        int b=20;
+        int sum=a+b;
+        System.out.println("Sum:-"+sum);
+        
+        
     }
 }
