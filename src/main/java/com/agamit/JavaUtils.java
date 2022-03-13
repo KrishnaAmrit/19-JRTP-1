7ad9dfa1470537b557888eb0f167280fa25c5ab3
ak203oops@gmail.com <ak203oops@gmail.com>
2022-03-13 19:46:29 +0530

    add method for pro

--- src/main/java/com/agamit/JavaUtils.java
+++ src/main/java/com/agamit/JavaUtils.java
@@ -6,22 +6,6 @@ public class JavaUtils {
 
 	}
 
-	private void sysout() {
-
-		int a = 10;
-		int b = 30;
-
-		// bux fix 404
-		System.out.println("Hi Git stash");
-
-		// change for bug fix 101
-		double d1 = 10.09;
-		double d2 = 12.09;
-		double d3 = 13.09;
-
-		int b3 = 0;
-
-	}
 	public String decode() {
 		int b = 30;
 
