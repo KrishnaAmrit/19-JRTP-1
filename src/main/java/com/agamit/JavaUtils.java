f0db2d40d83f328725f1590c591808bafc0ca05b
ak203oops@gmail.com <ak203oops@gmail.com>
2022-03-13 19:30:23 +0530

    add AGAM KRISHNA@DESKTOP-VMSRF1M MINGW64 /c/Workspace/19-JRTP-7AM/GIT-Repositry-1/Proj-1-MavenProjDemo (main)

--- src/main/java/com/agamit/JavaUtils.java
+++ src/main/java/com/agamit/JavaUtils.java
@@ -3,25 +3,41 @@ package com.agamit;
 public class JavaUtils {
 	public static void main(String[] args) {
 		System.out.println("hi");
-		
-	}
-private void sysout() {
-	
-	int a=10;
-	int b=30;
 
-	//bux fix 404
-	System.out.println("Hi Git stash");
+	}
+
+	private void sysout() {
+
+		int a = 10;
+		int b = 30;
+
+		// bux fix 404
+		System.out.println("Hi Git stash");
+
+		// change for bug fix 101
+		double d1 = 10.09;
+		double d2 = 12.09;
+		double d3 = 13.09;
+
+		int b3 = 0;
+
+	}
 	
-	//change for bug fix 101
-	double d1=10.09;
-	double d2=12.09;
-	double d3=13.09;
-	
-int b3=0;
-}
-	
-	
-	
+	public int decode() {
+		int b = 30;
+
+		// bux fix 404
+		System.out.println("Hi Git stash");
+
+		// change for bug fix 101
+		double d1 = 10.09;
+		double d2 = 12.09;
+		double d3 = 13.09;
+
+		int b3 = 0;
+
+
+		return 1;
+	}
 
 }
